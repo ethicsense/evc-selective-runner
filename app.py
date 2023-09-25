dadb732347d02b84f5bbb074791ec3a99540b6e2
ethicsense <gycksdl94@gmail.com>
2023-09-25 17:54:38 +0900

    model choice UI

--- app.py
+++ app.py
@@ -16,38 +16,6 @@ def print_model(model_type, model_name):
     return out, model
 
 
-def show_models(model_type):
-    if model_type == "ResNet":
-        lst = [
-            "resnet18",
-            "resnet34",
-            "resnet50",
-            "resnet101",
-            "resnet152"
-        ]
-    elif model_type == "EfficientNet":
-        lst = [
-            "efficientnet_b0",
-            "efficientnet_b1",
-            "efficientnet_b2",
-            "efficientnet_b3",
-            "efficientnet_b4",
-            "efficientnet_b5",
-            "efficientnet_b6",
-            "efficientnet_b7"
-        ]
-    elif model_type == "MobileNet":
-        lst = [
-            "mobilenet_v3_small",
-            "mobilenet_v3_large"
-        ]
-    elif model_type == "YOLOv8":
-        lst = [
-            "yolov8"
-        ]
-    
-    return lst
-
 
 def get_weights(f):
 
@@ -58,16 +26,13 @@ def run_model():
     pass
 
 
-def run_gradio():
-    with gr.Blocks() as demo:
-        pass
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument(
         '--server_name',
         type=str,
-        defualt='0.0.0.0'
+        default='0.0.0.0'
     )
     parser.add_argument(
         '--server_port',
@@ -76,7 +41,55 @@ if __name__ == "__main__":
     )
     args=parser.parse_args()
 
+
+    with gr.Blocks() as demo:
+        radio = gr.Radio(label="Model Class", choices=["ResNet", "EfficientNet", "MobileNet", "YOLOv8"])
+        model_list = gr.Dropdown(label="Models Available", choices=[], interactive=True)
+        model_map = {
+            "ResNet" : [
+                "resnet18",
+                "resnet34",
+                "resnet50",
+                "resnet101",
+                "resnet152"
+            ],
+            "EfficientNet" : [
+                "efficientnet_b0",
+                "efficientnet_b1",
+                "efficientnet_b2",
+                "efficientnet_b3",
+                "efficientnet_b4",
+                "efficientnet_b5",
+                "efficientnet_b6",
+                "efficientnet_b7"
+            ],
+            "MobileNet" : [
+                "mobilenet_v3_small",
+                "mobilenet_v3_large"
+            ],
+            "YOLOv8" : [
+                "yolov8n",
+                "yolov8s",
+                "yolov8m",
+                "yolov8l",
+                "yolov8x"
+            ]
+        }
+
+        def filter_models(choice):
+            if choice in model_map.keys():
+                return gr.Dropdown.update(
+                    choices=model_map[choice], value=model_map[choice][1]
+                )
+            else:
+                return gr.Dropdown.update(visible=False)
+        
+        radio.change(filter_models, inputs=radio, outputs=model_list)
+
+
+
     demo.launch(
         server_name=args.server_name,
-        server_port=args.server_port
+        server_port=args.server_port,
+        debug=True
     )
